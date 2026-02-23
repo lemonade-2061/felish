b454980caae3407f38b3eeb394df914ce2622742
lemonade-2061 <lemonenergy.2061.7.29@gmail.com>
2026-02-24 00:42:13 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("felish>");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        let input = input.trim();
+
+        println!("入力: {}", input);
+    }
 }
